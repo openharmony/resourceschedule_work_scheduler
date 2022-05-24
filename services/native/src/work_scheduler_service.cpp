508c8f7465521ba4e9ac507d4aeaf1f93d9d7187
zhutianyi <zhutianyi2@huawei.com>
2022-05-24 21:10:12 +0800

    修复addwork空指针异常
    Signed-off-by: zhutianyi <zhutianyi2@huawei.com>

--- services/native/src/work_scheduler_service.cpp
+++ services/native/src/work_scheduler_service.cpp
@@ -457,8 +457,8 @@ bool WorkSchedulerService::ShellDump(const vector<string> &dumpOption, vector<st
         return false;
     }
     if (dumpOption.size() < DUMP_PARAM_INDEX + 1) {
-        WS_HILOGI("Dump need at least three params.");
-        dumpInfo.push_back(string("dump need at least three params."));
+        WS_HILOGI("Dump need at least two params.");
+        dumpInfo.push_back(string("dump need at least two params."));
         return false;
     }
     if (dumpOption[DUMP_PARAM_INDEX] == ALL_INFO) {
