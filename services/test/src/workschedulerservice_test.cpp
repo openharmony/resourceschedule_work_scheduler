827726ee0f45709db665edc1b25537dd64184892
chenliangxing <chenliangxing@huawei.com>
2023-11-09 01:18:43 +0000

    update
    Signed-off-by: chenliangxing <chenliangxing@huawei.com>

--- services/test/src/workschedulerservice_test.cpp
+++ services/test/src/workschedulerservice_test.cpp
@@ -25,20 +25,68 @@ namespace OHOS {
 namespace WorkScheduler {
 class WorkSchedulerServiceTest : public testing::Test {
 public:
-    static void SetUpTestCase() {}
+    static void SetUpTestCase();
     static void TearDownTestCase() {}
     void SetUp() {}
     void TearDown() {}
+    static std::shared_ptr<WorkSchedulerService> workSchedulerService_;
 };
+
+std::shared_ptr<WorkSchedulerService> workSchedulerService_ = nullptr;
+
+void WorkSchedulerServiceTest::SetUpTestCase()
+{
+    workSchedulerService_ = std::make_shared<WorkSchedulerService>();
+}
+
 /**
  * @tc.name: onStart_001
- * @tc.desc: Test BackgroundTaskMgrService service ready.
+ * @tc.desc: Test WorkSchedulerService OnStart.
  * @tc.type: FUNC
- * @tc.require: SR000GGTET SR000GMUG8 AR000GH86O AR000GH86Q AR000GMUIA AR000GMUHN
+ * @tc.require: I8F08T
  */
 HWTEST_F(WorkSchedulerServiceTest, onStart_001, TestSize.Level1)
 {
-    
+    workSchedulerService_->OnStart();
+    EXPECT_NE(workSchedulerService_, nullptr);
+}
+
+/**
+ * @tc.name: onStop_001
+ * @tc.desc: Test WorkSchedulerService OnStop.
+ * @tc.type: FUNC
+ * @tc.require: I8F08T
+ */
+HWTEST_F(WorkSchedulerServiceTest, onStop_001, TestSize.Level1)
+{
+    workSchedulerService_->OnStop();
+    EXPECT_NE(workSchedulerService_, nullptr);
+}
+
+/**
+ * @tc.name: startWork_001
+ * @tc.desc: Test WorkSchedulerService onStop.
+ * @tc.type: FUNC
+ * @tc.require: I8F08T
+ */
+HWTEST_F(WorkSchedulerServiceTest, startWork_001, TestSize.Level1)
+{
+    WorkInfo workinfo = WorkInfo();
+    auto ret = workSchedulerService_->StartWork(workinfo);
+    EXPECT_NE(ret, 0);
+}
+
+/**
+ * @tc.name: stopWork_001
+ * @tc.desc: Test WorkSchedulerService stopWork.
+ * @tc.type: FUNC
+ * @tc.require: I8F08T
+ */
+HWTEST_F(WorkSchedulerServiceTest, stopWork_001, TestSize.Level1)
+{
+    WorkInfo workinfo = WorkInfo();
+    auto ret = workSchedulerService_->StopWork(workinfo);
+    EXPECT_NE(ret, 0);
 }
 }  // namespace BackgroundTaskMgr
 }  // namespace OHOS
