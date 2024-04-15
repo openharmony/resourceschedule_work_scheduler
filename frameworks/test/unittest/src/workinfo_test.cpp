a50c45e0108d120139e090dadaddcd0d0ee275a6
hxf <goooood_idea@163.com>
2024-04-15 17:29:07 +0800

    通用规范告警修改 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

--- frameworks/test/unittest/src/workinfo_test.cpp
+++ frameworks/test/unittest/src/workinfo_test.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define private public
 #include "workinfo_test.h"
 #include <message_parcel.h>
 
--- interfaces/kits/js/napi/work_scheduler_extension/work_scheduler_extension_ability_module.cpp
+++ interfaces/kits/js/napi/work_scheduler_extension/work_scheduler_extension_ability_module.cpp
@@ -20,8 +20,7 @@ extern const char _binary_work_scheduler_extension_ability_js_end[];
 extern const char _binary_work_scheduler_extension_ability_abc_start[];
 extern const char _binary_work_scheduler_extension_ability_abc_end[];
 
-extern "C" __attribute__((constructor))
-void NAPI_WorkSchedulerExtensionAbility_AutoRegister()
+extern "C" __attribute__((constructor)) void NAPI_WorkSchedulerExtensionAbility_AutoRegister()
 {
     auto moduleManager = NativeModuleManager::GetInstance();
     NativeModule newModuleInfo = {
@@ -32,8 +31,8 @@ void NAPI_WorkSchedulerExtensionAbility_AutoRegister()
     moduleManager->Register(&newModuleInfo);
 }
 
-extern "C" __attribute__((visibility("default")))
-void NAPI_WorkSchedulerExtensionAbility_GetJSCode(const char **buf, int32_t *bufLen)
+extern "C" __attribute__((visibility("default"))) void NAPI_WorkSchedulerExtensionAbility_GetJSCode(
+    const char **buf, int32_t *bufLen)
 {
     if (buf != nullptr) {
         *buf = _binary_work_scheduler_extension_ability_js_start;
@@ -46,8 +45,8 @@ void NAPI_WorkSchedulerExtensionAbility_GetJSCode(const char **buf, int32_t *buf
 }
 
 // WorkSchedulerExtension JS register
-extern "C" __attribute__((visibility("default")))
-void NAPI_WorkSchedulerExtensionAbility_GetABCCode(const char **buf, int32_t *buflen)
+extern "C" __attribute__((visibility("default"))) void NAPI_WorkSchedulerExtensionAbility_GetABCCode(
+    const char **buf, int32_t *buflen)
 {
     if (buf != nullptr) {
         *buf = _binary_work_scheduler_extension_ability_abc_start;
