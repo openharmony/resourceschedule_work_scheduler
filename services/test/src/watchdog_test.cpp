f9395c159f8a05ccc44044d8c5cb65d442e51ffb
hxf <goooood_idea@163.com>
2024-04-27 16:12:15 +0800

    通用告警修改 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

--- services/test/src/watchdog_test.cpp
+++ services/test/src/watchdog_test.cpp
@@ -17,6 +17,8 @@
 #include <gtest/gtest.h>
 
 #include "watchdog.h"
+#include "work_policy_manager.h"
+#include "work_scheduler_service.h"
 
 using namespace testing::ext;
 
@@ -36,9 +38,9 @@ std::shared_ptr<Watchdog> WatchdogTest::watchdog_ = nullptr;
 
 void WatchdogTest::SetUpTestCase()
 {
-    std::shared_ptr<WorkPolicyManager> service;
+    std::shared_ptr<WorkSchedulerService> workSchedulerService = std::make_shared<WorkSchedulerService>::GetInstance();
     std::shared_ptr<AppExecFwk::EventRunner> runner;
-    watchdog_ = std::make_shared<Watchdog>(service, runner);
+    watchdog_ = std::make_shared<Watchdog>(workSchedulerService->GetWorkPolicyManager(), runner);
 }
 
 /**
@@ -51,6 +53,62 @@ HWTEST_F(WatchdogTest, watchdog_001, TestSize.Level1)
 {
     bool result = watchdog_->AddWatchdog(1, 1);
     EXPECT_EQ(result, false);
+}
+
+/**
+ * @tc.name: watchdog_002
+ * @tc.desc: Test Watchdog RemoveWatchdog.
+ * @tc.type: FUNC
+ * @tc.require: I8JBRY
+ */
+HWTEST_F(WatchdogTest, watchdog_002, TestSize.Level1)
+{
+    bool result = watchdog_->AddWatchdog(1, 1);
+    EXPECT_EQ(result, false);
+    watchdog_->RemoveWatchdog(1);
+}
+
+/**
+ * @tc.name: watchdog_003
+ * @tc.desc: Test Watchdog ProcessEvent.
+ * @tc.type: FUNC
+ * @tc.require: I8JBRY
+ */
+HWTEST_F(WatchdogTest, watchdog_003, TestSize.Level1)
+{
+    AppExecFwk::InnerEvent::Pointer event = AppExecFwk::InnerEvent::Get(0);
+    event = nullptr;
+    watchdog_->ProcessEvent(event);
+    EXPECT_TRUE(event == nullptr);
+}
+
+/**
+ * @tc.name: watchdog_004
+ * @tc.desc: Test Watchdog ProcessEvent.
+ * @tc.type: FUNC
+ * @tc.require: I8JBRY
+ */
+HWTEST_F(WatchdogTest, watchdog_004, TestSize.Level1)
+{
+    AppExecFwk::InnerEvent::Pointer event = AppExecFwk::InnerEvent::Get(0);
+    watchdog_->ProcessEvent(event);
+    EXPECT_TRUE(event->GetInnerEventId() == 0);
+}
+
+/**
+ * @tc.name: watchdog_005
+ * @tc.desc: Test Watchdog ProcessEvent.
+ * @tc.type: FUNC
+ * @tc.require: I8JBRY
+ */
+HWTEST_F(WatchdogTest, watchdog_005, TestSize.Level1)
+{
+    AppExecFwk::InnerEvent::Pointer event = AppExecFwk::InnerEvent::Get(0);
+    watchdog_->service_ = nullptr;
+    watchdog_->ProcessEvent(event);
+    std::shared_ptr<WorkPolicyManager> service;
+    watchdog_->service_ = service;
+    EXPECT_TRUE(event->GetInnerEventId() == 0);
 }
 
 }
