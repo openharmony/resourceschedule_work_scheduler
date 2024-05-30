e450cf3c644b1d169e88eee4166fff418765f984
anchi <anchi1@huawei.com>
2024-05-30 20:02:30 +0800

    延迟任调度务适配分身
    Signed-off-by: anchi <anchi1@huawei.com>

--- services/native/src/work_conn_manager.cpp
+++ services/native/src/work_conn_manager.cpp
@@ -38,6 +38,7 @@ using namespace OHOS::HiviewDFX;
 namespace OHOS {
 namespace WorkScheduler {
 const std::string PARAM_APP_CLONE_INDEX_KEY = "ohos.extra.param.key.appCloneIndex";
+
 void WorkConnManager::AddConnInfo(string &workId, sptr<WorkSchedulerConnection> &connection)
 {
     std::lock_guard<std::mutex> lock(connMapMutex_);
@@ -96,7 +97,7 @@ bool WorkConnManager::StartWork(shared_ptr<WorkStatus> workStatus)
 
     Want want;
     want.SetElementName(workStatus->bundleName_, workStatus->abilityName_);
-    want.SetParam(, workStatus->workInfo->getAppIndex());
+    want.SetParam(PARAM_APP_CLONE_INDEX_KEY, workStatus->workInfo->getAppIndex());
     int32_t ret = abilityMgr_->ConnectAbility(want, connection, nullptr, workStatus->userId_);
     if (ret != ERR_OK) {
         WS_HILOGE("connect failed");
