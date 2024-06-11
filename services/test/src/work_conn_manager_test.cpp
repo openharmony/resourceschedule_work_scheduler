0c685de364b014940a60d0b9562aeebdd3d00f39
lipengfei <lipengfei53@huawei.com>
2024-06-11 06:56:22 +0000

    testcase fix
    Signed-off-by: lipengfei <lipengfei53@huawei.com>

--- services/test/src/work_conn_manager_test.cpp
+++ services/test/src/work_conn_manager_test.cpp
@@ -222,7 +222,7 @@ HWTEST_F(WorkConnManagerTest, StopWork_003, TestSize.Level1)
     sptr<WorkSchedulerConnection> connection(new (std::nothrow) WorkSchedulerConnection(workStatus->workInfo_));
     myWorkConnManager.AddConnInfo(workId, connection);
     bool ret = myWorkConnManager.StopWork(workStatus, false);
-    EXPECT_TRUE(ret);
+    EXPECT_TRUE(!ret);
 }
 
 /**
@@ -247,7 +247,7 @@ HWTEST_F(WorkConnManagerTest, StopWork_004, TestSize.Level1)
     sptr<WorkSchedulerConnection> connection(new (std::nothrow) WorkSchedulerConnection(workStatus->workInfo_));
     myWorkConnManager.AddConnInfo(workId, connection);
     bool ret = myWorkConnManager.StopWork(workStatus, true);
-    EXPECT_TRUE(ret);
+    EXPECT_TRUE(!ret);
 }
 
 /**
