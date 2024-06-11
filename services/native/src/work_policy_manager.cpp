141b235ce794b1e4adab60639ce6a018e0146142
lipengfei <lipengfei53@huawei.com>
2024-06-11 04:03:21 +0000

    update services/native/src/work_policy_manager.cpp.
    Signed-off-by: lipengfei <lipengfei53@huawei.com>

--- services/native/src/work_policy_manager.cpp
+++ services/native/src/work_policy_manager.cpp
@@ -225,7 +225,7 @@ bool WorkPolicyManager::StopWork(std::shared_ptr<WorkStatus> workStatus, int32_t
         RemoveFromReadyQueue(workStatus);
         hasCanceled = true;
     }
-    if (isTimeOut && (workStatus->GetStatus() == WorkStatus::Status::REMOVED) {
+    if (isTimeOut && (workStatus->GetStatus() == WorkStatus::Status::REMOVED)) {
         WS_HILOGI("disconect %{public}s when timeout", workStatus->workdId.c_ctr());
         workStatus->lastTimeout_ = isTimeOut;
         workConnManager_->StopWork(workStatus, isTimeOut);
