d96a8ddbadf41d273b822385891d98c8cbbbba42
hxf <goooood_idea@163.com>
2024-07-10 16:58:51 +0800

    延迟任务质量加固 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

--- services/native/src/work_conn_manager.cpp
+++ services/native/src/work_conn_manager.cpp
@@ -169,7 +169,7 @@ bool WorkConnManager::StopWork(shared_ptr<WorkStatus> workStatus, bool isTimeOut
             workStatus->uid_, "PID", pid, "NAME", workStatus->bundleName_, "WORKID", workStatus->workId_,
             "REASON", isTimeOut, "DURATION", workStatus->duration_);
 #ifdef DEVICE_STANDBY_ENABLE
-        WS_HILOGI("OnWorkStop uid: %{public}d, duration:%{public}zu, startTime:%{public}zu",
+        WS_HILOGI("OnWorkStop uid: %{public}d, duration:%{public}" PRIu64 ", startTime:%{public}" PRIu64,
             workStatus->uid_, workStatus->duration_, workStatus->workStartTime_);
         DevStandbyMgr::StandbyServiceClient::GetInstance().ReportWorkSchedulerStatus(false,
             workStatus->uid_, workStatus->bundleName_);
