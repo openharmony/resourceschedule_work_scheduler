fff3cea0df44cceed84210b1aa802981f6548931
hxf <goooood_idea@163.com>
2024-10-19 18:45:07 +0800

    延迟任务TDD补充 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

---
+++ services/test/src/policy/power_mode_policy_test.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <functional>
+#include <gtest/gtest.h>
+
+#include "policy/power_mode_policy.h"
+#include "work_policy_manager.h"
+#include "work_scheduler_service.h"
+#include "battery_srv_client.h"
+#include "power_mgr_client.h"
+#include "power_mode_info.h"
+
+
+using namespace testing::ext;
+
+namespace OHOS {
+namespace PowerMgr {
+BatteryChargeState BatterySrvClient::GetChargingStatus()
+{
+    return BatteryChargeState::CHARGE_STATE_NONE;
+}
+}
+namespace WorkScheduler {
+class PowerModePolicyTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase() {};
+    void SetUp() {};
+    void TearDown() {};
+    static std::shared_ptr<WorkPolicyManager> workPolicyManager_;
+    static std::shared_ptr<PowerModePolicy> powerModePolicy_;
+};
+
+std::shared_ptr<PowerModePolicy> PowerModePolicyTest::powerModePolicy_ = nullptr;
+std::shared_ptr<WorkPolicyManager> PowerModePolicyTest::workPolicyManager_ = nullptr;
+
+void PowerModePolicyTest::SetUpTestCase()
+{
+    std::shared_ptr<WorkSchedulerService> workSchedulerService_ = std::make_shared<WorkSchedulerService>();
+    workSchedulerService_->Start();
+    workPolicyManager_ = std::make_shared<WorkPolicyManager>(workSchedulerService_);
+    powerModePolicy_ = std::make_shared<PowerModePolicy>(workPolicyManager_);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_001
+ * @tc.desc: Test PowerModePolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(PowerModePolicyTest, GetPolicyMaxRunning_001, TestSize.Level1)
+{
+    PowerMgr::PowerMgrClient::GetInstance().SetDeviceMode(PowerMgr::PowerMode::NORMAL_MODE);
+    int32_t ret = powerModePolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 3);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_002
+ * @tc.desc: Test PowerModePolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(PowerModePolicyTest, GetPolicyMaxRunning_002, TestSize.Level1)
+{
+    PowerMgr::PowerMgrClient::GetInstance().SetDeviceMode(PowerMgr::PowerMode::PERFORMANCE_MODE);
+    int32_t ret = powerModePolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 3);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_003
+ * @tc.desc: Test PowerModePolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(PowerModePolicyTest, GetPolicyMaxRunning_003, TestSize.Level1)
+{
+    PowerMgr::PowerMgrClient::GetInstance().SetDeviceMode(PowerMgr::PowerMode::POWER_MODE_MIN);
+    int32_t ret = powerModePolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 3);
+}
+
+/**
+ * @tc.name: GetPolicyName_001
+ * @tc.desc: Test PowerModePolicy GetPolicyName.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(PowerModePolicyTest, GetPolicyName_001, TestSize.Level1)
+{
+    std::string ret = powerModePolicy_->GetPolicyName();
+    EXPECT_EQ(ret, "POWER_MODE_POLICY");
+}
+}
+}
---
+++ services/test/src/policy/thermal_policy_test.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <functional>
+#include <gtest/gtest.h>
+
+#include "policy/thermal_policy.h"
+#include "work_policy_manager.h"
+#include "work_scheduler_service.h"
+#include "thermal_mgr_client.h"
+
+using namespace testing::ext;
+
+namespace {
+OHOS::PowerMgr::ThermalLevel thermalLevel_;
+void MockProcess(OHOS::PowerMgr::ThermalLevel thermlLevel)
+{
+    thermalLevel_ = thermlLevel;
+}
+}
+
+namespace OHOS {
+namespace PowerMgr {
+ThermalLevel ThermalMgrClient::GetThermalLevel()
+{
+    return thermalLevel_;
+}
+}
+namespace WorkScheduler {
+class ThermalPolicyTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase() {};
+    void SetUp() {};
+    void TearDown() {};
+    static std::shared_ptr<WorkPolicyManager> workPolicyManager_;
+    static std::shared_ptr<ThermalPolicy> thermalPolicy_;
+};
+
+std::shared_ptr<ThermalPolicy> ThermalPolicyTest::thermalPolicy_ = nullptr;
+std::shared_ptr<WorkPolicyManager> ThermalPolicyTest::workPolicyManager_ = nullptr;
+
+void ThermalPolicyTest::SetUpTestCase()
+{
+    std::shared_ptr<WorkSchedulerService> workSchedulerService_ = std::make_shared<WorkSchedulerService>();
+    workSchedulerService_->Start();
+    workPolicyManager_ = std::make_shared<WorkPolicyManager>(workSchedulerService_);
+    thermalPolicy_ = std::make_shared<ThermalPolicy>(workPolicyManager_);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_001
+ * @tc.desc: Test ThermalPolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(ThermalPolicyTest, GetPolicyMaxRunning_001, TestSize.Level1)
+{
+    MockProcess(PowerMgr::ThermalLevel::WARM);
+    int32_t ret = thermalPolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_002
+ * @tc.desc: Test ThermalPolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(ThermalPolicyTest, GetPolicyMaxRunning_002, TestSize.Level1)
+{
+    MockProcess(PowerMgr::ThermalLevel::NORMAL);
+    int32_t ret = thermalPolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 1);
+}
+
+/**
+ * @tc.name: GetPolicyMaxRunning_003
+ * @tc.desc: Test ThermalPolicy GetPolicyMaxRunning.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(ThermalPolicyTest, GetPolicyMaxRunning_003, TestSize.Level1)
+{
+    MockProcess(PowerMgr::ThermalLevel::COOL);
+    int32_t ret = thermalPolicy_->GetPolicyMaxRunning();
+    EXPECT_EQ(ret, 3);
+}
+
+/**
+ * @tc.name: GetPolicyName_001
+ * @tc.desc: Test ThermalPolicy GetPolicyName.
+ * @tc.type: FUNC
+ * @tc.require: I974IQ
+ */
+HWTEST_F(ThermalPolicyTest, GetPolicyName_001, TestSize.Level1)
+{
+    std::string ret = thermalPolicy_->GetPolicyName();
+    EXPECT_EQ(ret, "THERMAL_POLICY");
+}
+}
+}
