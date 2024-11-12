0ff51301319817b3b723f401ab6dfbc638bd164d
hxf <goooood_idea@163.com>
2024-11-12 10:21:21 +0800

    延迟任务定时任务count<=0时刷新固化文件 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

--- services/native/src/work_scheduler_service.cpp
+++ services/native/src/work_scheduler_service.cpp
@@ -700,6 +700,9 @@ bool WorkSchedulerService::StopWorkInner(std::shared_ptr<WorkStatus> workStatus,
     if (workPolicyManager_->StopWork(workStatus, uid, needCancel, isTimeOut)) {
         workQueueManager_->CancelWork(workStatus);
     }
+    if (!isTimeOut) {
+        workPolicyManager_->RemoveWatchDog(workStatus);
+    }
     return true;
 }
 
@@ -802,6 +805,11 @@ void WorkSchedulerService::UpdateWorkBeforeRealStart(std::shared_ptr<WorkStatus>
     work->UpdateTimerIfNeed();
     if (work->NeedRemove()) {
         workQueueManager_->RemoveWork(work);
+        if (work->persisted_ && !work->IsRepeating()) {
+            std::lock_guard<ffrt::mutex> lock(mutex_);
+            persistedMap_.erase(work->workId_);
+            RefreshPersistedWorks();
+        }
     }
 }
 
