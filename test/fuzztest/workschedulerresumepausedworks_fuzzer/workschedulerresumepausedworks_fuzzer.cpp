4b2a01362bf87170cc48623e8dc6a25379157448
hxf <goooood_idea@163.com>
2024-11-23 11:23:23 +0800

    延迟任务FUZZ补充 Signed-off-by: hxf <haoxiaofeng2@huawei.com>

---
+++ test/fuzztest/workschedulerresumepausedworks_fuzzer/workschedulerresumepausedworks_fuzzer.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "workschedulerresumepausedworks_fuzzer.h"
+
+#include "iwork_sched_service_ipc_interface_code.h"
+#include "work_scheduler_service.h"
+#include "work_sched_common.h"
+#include "work_condition.h"
+
+
+void OHOS::RefBase::DecStrongRef(void const* obj) {}
+
+namespace OHOS {
+namespace WorkScheduler {
+    const std::u16string WORK_SCHEDULER_STUB_TOKEN = u"ohos.workscheduler.iworkschedservice";
+    static std::shared_ptr<WorkSchedulerService> workSchedulerService_;
+
+    bool WorkSchedulerService::GetUidByBundleName(const std::string &bundleName, int32_t &uid)
+    {
+        return true;
+    }
+
+    bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+    {
+        MessageParcel dataMessageParcel;
+        dataMessageParcel.WriteInterfaceToken(WORK_SCHEDULER_STUB_TOKEN);
+        dataMessageParcel.WriteBuffer(data, size);
+        dataMessageParcel.RewindRead(0);
+        MessageParcel reply;
+        MessageOption option;
+        workSchedulerService_ = DelayedSingleton<WorkSchedulerService>::GetInstance();
+        uint32_t code = static_cast<int32_t>(IWorkSchedServiceInterfaceCode::RESUME_PAUSED_WORKS);
+        int32_t uid = 100;
+        if (!dataMessageParcel.WriteInt32(uid)) {
+            return false;
+        }
+        workSchedulerService_->OnStart();
+        workSchedulerService_->InitBgTaskSubscriber();
+        if (!workSchedulerService_->ready_) {
+            workSchedulerService_->ready_ = true;
+        }
+        if (workSchedulerService_->checkBundle_) {
+            workSchedulerService_->checkBundle_ = false;
+        }
+        workSchedulerService_->OnRemoteRequest(code, dataMessageParcel, reply, option);
+        workSchedulerService_->OnStop();
+        return true;
+    }
+} // WorkScheduler
+} // OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::WorkScheduler::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
