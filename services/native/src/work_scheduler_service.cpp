668d92278dbea519c66fb176ea7406ae58b56b42
redlotus127 <yyflfszs@qq.com>
2025-02-25 12:05:49 +0000

    update services/native/src/work_scheduler_service.cpp.
    Signed-off-by: redlotus127 <yyflfszs@qq.com>

--- services/native/src/work_scheduler_service.cpp
+++ services/native/src/work_scheduler_service.cpp
@@ -1286,7 +1286,7 @@ void WorkSchedulerService::InitDeviceStandyRestrictlist()
 {
 #ifdef DEVICE_STANDBY_ENABLE
     std::vector<DevStandbyMgr::AllowInfo> allowInfoArray;
-    auto res = DevStandbyMgr::StandbyServiceClient::GetInstance().GetRestrictlist(
+    auto res = DevStandbyMgr::StandbyServiceClient::GetInstance().GetRestrictList(
         DevStandbyMgr::AllowType::WORK_SCHEDULER, allowInfoArray, DevStandbyMgr::ReasonCodeEnum::REASON_APP_API);
     if (res != ERR_OK) {
         WS_HILOGE("GetRestrictlist fail");
