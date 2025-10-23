268fd6763ace40039e04964d8d1f8f75f8407758
let_it_rot <gaojiaqi7@h-partners.com>
2025-10-23 09:26:00 +0800

    fix bug
    Signed-off-by: let_it_rot <gaojiaqi7@h-partners.com>

--- services/native/src/work_policy_manager.cpp
+++ services/native/src/work_policy_manager.cpp
@@ -1019,7 +1019,7 @@ void WorkPolicyManager::DiscreteScheduled(std::shared_ptr<WorkStatus> topWork)
     }
     constexpr int32_t MAX_DELAY_SECOND = 120;
     constexpr int32_t MILLISECOND = 1000;
-    int32_t seed = (unsigned)time(NULL);
+    uint32_t seed = static_cast<uint32_t>(time(NULL));
     srand(seed);
     int32_t delay = rand() % MAX_DELAY_SECOND + 1;
     handler->PostTask(task, delay * MILLISECOND);
