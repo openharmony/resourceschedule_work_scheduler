385f106b39c119a0523eeed969bc8d48cd63be2b
jusiayo <jusiyao1@huawei.com>
2026-08-10 17:47:10 +0800

    addbackgroundloader
    Signed-off-by: jusiayo <jusiyao1@huawei.com>

--- interfaces/kits/ets/taihe/background_loader/src/ani_constructor.cpp
+++ interfaces/kits/ets/taihe/background_loader/src/ani_constructor.cpp
@@ -12,8 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
- #include "ohos.resourceschedule.backgroundLoader.ani.hpp"
+#include "ohos.resourceschedule.backgroundLoader.ani.hpp"
 
 #if __has_include(<ani.h>)
 #include <ani.h>
